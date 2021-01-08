4a894c157ab60367ae62d06c743ebe96e42fb55e
gretanorv <gretanorv@gmail.com>
2021-01-08 18:30:42 +0200

    delete commented sql

--- index.php
+++ index.php
@@ -27,19 +27,6 @@
         $username = 'root';
         $password = 'mysql';
 
-        //TODO:: instructions for db initiation:: WATCH 
-        //create connection
-        // $conn = mysqli_connect($server_name, $username, $password);
-        // if (!$conn) (die("Connection failed: " . mysqli_connect_error()));
-        // 
-        //create database
-        // $sql = 'CREATE DATABASE personalo_valdymo_sistema';
-        // if (mysqli_query($conn, $sql)) {
-        //     echo "Database created successfully";
-        // } else {
-        //     echo "Error creating database: " . mysqli_error($conn);
-        // }
-
         $db_name = 'personalo_valdymo_sistema';
         $conn = mysqli_connect($server_name, $username, $password, $db_name);
 
@@ -115,44 +102,6 @@
 
         print("<h2 class='main__title'>{$title}</h2>");
         print_table($conn, $sql);
-
-
-        // //create tables
-        // $sql = 'CREATE TABLE projektai (
-        //     id int(6) auto_increment primary key,
-        //     projekto_pavadinimas varchar(30)
-        // )';
-        // if (mysqli_query($conn, $sql)) echo "Table projektai created successfully";
-
-        // $sql = 'CREATE TABLE darbuotojai (
-        //     id int(6) auto_increment primary key,
-        //     vardas varchar(30),
-        //     pavarde varchar(30),
-        //     projekto_id int,
-        //     FOREIGN KEY (projekto_id) REFERENCES projektai(id) 	
-        // )';
-        // if (mysqli_query($conn, $sql)) echo "Table darbuotojai created successfully";
-
-        // //isert values
-        // $sql = "INSERT INTO projektai VALUES
-        //     (1, 'X'),
-        //     (2, 'Y'),
-        //     (3, 'Z')";
-        // if (mysqli_query($conn, $sql)) echo "Values were successfully included into projektai";
-
-        // $sql = "INSERT INTO darbuotojai VALUES
-        // (1, 'A', 'Aaa', 1),
-        // (2, 'B', 'Bbb', 1),
-        // (3, 'C', 'Ccc', 1),
-        // (4, 'D', 'Ddd', 1),
-        // (5, 'E', 'Eee', 1),
-        // (6, 'F', 'Fff', 2),
-        // (7, 'G', 'Ggg', 2),
-        // (8, 'H', 'Hhh', 2),
-        // (9, 'I', 'Iii', 2),
-        // (10, 'J', 'Jjj', 3),
-        // (11, 'K', 'Kkk', 3)";
-        // if (mysqli_query($conn, $sql)) echo "Values were successfully included into darbuotojai";
 
 
         //projektai ir darbuotojai
