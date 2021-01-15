00636dc07be8617919a192852c47afda607395cc
gretanorv <gretanorv@gmail.com>
2021-01-15 10:39:31 +0200

    on delete set null

--- personalo_valdymo_sistema__sql (1).sql
+++ personalo_valdymo_sistema__sql (1).sql
@@ -32,7 +32,7 @@ CREATE TABLE `darbuotojai` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `projekto_id` (`projekto_id`),
-  CONSTRAINT `darbuotojai_ibfk_1` FOREIGN KEY (`projekto_id`) REFERENCES `projektai` (`id`)
+  CONSTRAINT `darbuotojai_ibfk_1` FOREIGN KEY (`projekto_id`) REFERENCES `projektai` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -80,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-09 14:48:29
+-- Dump completed on 2021-01-15 10:37:53
